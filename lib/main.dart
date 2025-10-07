83af9f7e5002e1e9f24b39d90f26162055f1285d
nethmalgeesaraux <nethmalpaypal@gmail.com>
2025-10-07 22:15:57 +0530

    Widgets title,broundColor,actions

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,9 +9,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "new app", 
-      home: Scaffold()
-      );
+    return MaterialApp(
+      title: "new app",
+      home: Scaffold(
+        appBar: AppBar(
+          title:const Text("Flutter App"),
+          backgroundColor: Colors.amberAccent,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.notifications_active),
+              onPressed: () {
+                },
+              ),
+            ],
+          ),
+        ),
+    );
   }
 }
