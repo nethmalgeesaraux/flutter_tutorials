7264d2d07772becff2f396ee396015e3c61dfc71
nethmalgeesaraux <nethmalpaypal@gmail.com>
2025-10-11 23:28:29 +0530

    Mobile App

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,28 +10,68 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "new app",
+      debugShowCheckedModeBanner: false,
+      title: "Space app",
       home: Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
-          title: const Text("Flutter App"),
-          backgroundColor: Colors.amberAccent,
+          backgroundColor: Colors.black,
+          shadowColor: Colors.red,
+          elevation: 5,
+          title: const Text(
+            "BLACK HOLE",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red),
+          ),
           actions: [
             IconButton(
-              icon: const Icon(Icons.notifications_active),
               onPressed: () {},
+              icon: const Icon(Icons.menu, color: Colors.white),
             ),
           ],
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 100, left: 100, right: 100),
-              child: Image.asset("assets/1.jpg", height: 200),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Space Detales",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Center(child: Image.asset("assets/2.jpg", height: 300, scale: 5)),
+                Text(
+                  "Black holes are regions of spacetime where gravity is so strong that nothing, not even light, can escape from them. They are formed when massive stars collapse under their own gravity at the end of their life cycles. Black holes can vary in size, from small ones with a few times the mass of our Sun to supermassive black holes found at the centers of galaxies, which can have masses equivalent to billions of Suns. The boundary surrounding a black hole is called the event horizon, beyond which nothing can return. Black holes play a crucial role in our understanding of physics, particularly in the realms of general relativity and quantum mechanics.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                //btn
+                Center(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Text(
+                      "Explore More",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-
-            const SizedBox(height: 200),
-          ],
+          ),
         ),
       ),
     );
